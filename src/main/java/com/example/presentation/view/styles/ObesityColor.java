420346250c3074528663a0b276caf0770638f0ed
sosuisen <hidekazu.kubota@gmail.com>
2025-08-25 21:52:04 +0900

    fix: change low obesity color to light blue

--- src/main/java/com/example/presentation/view/styles/ObesityColor.java
+++ src/main/java/com/example/presentation/view/styles/ObesityColor.java
@@ -9,7 +9,7 @@ public class ObesityColor {
     public static String getLightColor(ObesityCategory category) {
         return switch (category) {
             case NONE -> "#90a0a0";
-            case LOW -> "#ffe0c0";
+            case LOW -> "#e0f0ff";
             case NORMAL -> "#d0f0e0";
             case HIGH -> "#ffd0d0";
         };
@@ -18,7 +18,7 @@ public class ObesityColor {
     public static String getDarkColor(ObesityCategory category) {
         return switch (category) {
             case NONE -> "#507070";
-            case LOW -> "#d07000";
+            case LOW -> "#00a0d0";
             case NORMAL -> "#009030";
             case HIGH -> "#900000";
         };
