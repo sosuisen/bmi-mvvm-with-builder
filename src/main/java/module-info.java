07795bc136935a22aba24bfa788c69ab39118298
sosuisen <hidekazu.kubota@gmail.com>
2025-09-07 16:48:05 +0900

    feat: add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.app {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires io.github.sosuisen.api.jfxbuilder.controls;
+    requires io.github.sosuisen.api.jfxbuilder.graphics;
+
+    requires org.jooq;
+
+    exports com.example.repository.jooq.tables.records to org.jooq;
+    exports com.example.domain.model to org.jooq;
+
+    opens com.example.presentation to javafx.graphics;
+    opens com.example.main to javafx.graphics;
+}
